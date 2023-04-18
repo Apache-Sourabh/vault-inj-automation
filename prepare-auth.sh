97ec6c0e1cedda3a8dfbccccb9189c76c8d514a8
sourabh <sourabh@ubuntulocal.myguest.virtualbox.org>
2023-04-18 23:19:49 +0530

    vault kubernetes auth method added

---
+++ prepare-auth.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+VAULT_PROTOCOL="http"
+VAULT_URL="localhost:8200"
+export VAULT_TOKEN="$1"
+VAULT_PATH=("vault-client" "keycloak-client" "grafana-client" "database")
+
+K8s_Token=""
+k8s_CA=""
+
+final_ca=$(echo "$k8s_CA" | awk 'NF {sub(/\r/, ""); printf "%s\\n",$0;}' )
+
+
+
+TOKEN_LOOKUP=$(curl -s --header "X-Vault-Token: $VAULT_TOKEN" --request POST --data '{ "token": "'"$VAULT_TOKEN"'" }' $VAULT_PROTOCOL://$VAULT_URL/v1/auth/token/lookup)
+TOKEN_VALIDATE=$(echo $TOKEN_LOOKUP | jq -r .data.display_name)
+
+
+
+curl --request POST --header "X-Vault-Token: $VAULT_TOKEN" $VAULT_PROTOCOL://$VAULT_URL/v1/sys/policy/lila-acl-policy --data '{"policy":"path \"lila/'"*"'\" {\n capabilities = [\"list\",\"read\"]\n}"}'
+
+curl --request POST --header "X-Vault-Token:  $VAULT_TOKEN" --data '{"type":"kubernetes","description":"kubernetes auth"}'  $VAULT_PROTOCOL://$VAULT_URL/v1/sys/auth/kubernetes 
+
+curl --request POST --header "X-Vault-Token:  $VAULT_TOKEN" --request POST --data '{ "bound_service_account_names": "vault-auth", "bound_service_account_namespaces": "default","policies": ["lila-acl-policy"]}' $VAULT_PROTOCOL://$VAULT_URL/v1/auth/kubernetes/role/lila-role
+
+curl --header "X-Vault-Token: $VAULT_TOKEN" --request POST --data '{ "kubernetes_host": "http://kubernetes.default.svc.local", "kubernetes_ca_cert": "'"$K8s_Token"'", "pem_keys": "'"$final_ca"'"
+}' $VAULT_PROTOCOL://$VAULT_URL/v1/auth/kubernetes/config

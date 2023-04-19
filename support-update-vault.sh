6211688d0d3dfedc725f05c6f69c900a2dae7a4d
sourabh <sourabh@ubuntulocal.myguest.virtualbox.org>
2023-04-19 23:54:35 +0530

    code updated

--- support-update-vault.sh
+++ support-update-vault.sh
@@ -17,50 +17,56 @@ KEYCLOAK_CLIENT_NAMES=("argo" "grafana-oauth" "hashicorpvault" "kubeapps")
 VAULT_PROTOCOL="http"
 VAULT_URL="localhost:8200"
 export VAULT_TOKEN="$3"
-VAULT_PATH=("vault-client" "keycloak-client" "grafana-client" "database")
-Vault_Data=("grafana" "keycloak" "vault" "database")
 APP_NAME="argo"
 #---------------------#
 
-#Function to validate provided input
-check_input () {
-
-    if [ $# -lt 2 ]; then
+    if [ $# -lt 3 ]; then
         echo "####################################################" 
         echo "One or more argument is missing."
         echo 'script execution: "sh update-vault.sh <keycloak-admin-username> <keycloak-admin-password> <vault-root-token>"'
+        echo "####################################################"
         exit
     fi
 
-    TOKEN_LOOKUP=$(curl -s --header "X-Vault-Token: $VAULT_TOKEN" --request POST --data '{ "token": "'"$VAULT_TOKEN"'" }' $VAULT_PROTOCOL://$VAULT_URL/v1/auth/token/lookup)
-    TOKEN_VALIDATE=$(echo $TOKEN_LOOKUP | jq -r .data.display_name)
+    unseal_status=$(curl -s $VAULT_PROTOCOL://$VAULT_URL/v1/sys/seal-status | jq -r .sealed)
 
+    if [ "$unseal_status" == "false" ]; then
+        
+        TOKEN_LOOKUP=$(curl -s --header "X-Vault-Token: $VAULT_TOKEN" --request POST --data '{ "token": "'"$VAULT_TOKEN"'" }' $VAULT_PROTOCOL://$VAULT_URL/v1/auth/token/lookup)
+        TOKEN_VALIDATE=$(echo $TOKEN_LOOKUP | jq -r .data.display_name)
 
-    if [ $TOKEN_VALIDATE == "root" ]; then
+        if [ $TOKEN_VALIDATE == "root" ]; then
+            echo ""
+            echo "####################################################"
+            echo "*** Vault login success ***"
+            echo "####################################################"
+            echo ""
+        elif [ "$TOKEN_VALIDATE" == "null" ]; then
+            echo ""
+            echo "####################################################"
+            echo "Vault Login failed, please check vault details viz. Vault-URL and Root-Token"
+            echo "####################################################" 
+            echo ""
+            exit
+        fi
+    elif [ "$unseal_status" == "true" ]; then
         echo ""
         echo "####################################################"
-        echo "Vault login success"
+        echo "*** Vault is sealed ***"
         echo "####################################################"
-        echo ""
-        continue
-    elif [ "$TOKEN_VALIDATE" == "null" ]; then
-        echo ""
-        echo "####################################################"
-        echo "Vault Login failed, please check vault details viz. Vault-URL and Root-Token"
-        echo "####################################################" 
         echo ""
         exit
     fi
 
-    fetch_client_data
-}
-check_input
+    
+
+
 
 #Function to FETCH Keycloak Client data
 fetch_client_data () {
 
     echo "####################################################"
-    echo "***Fetching Bearer token***"
+    echo "*** Fetching Bearer token ***"
     echo "####################################################"
     echo ""
 
@@ -68,12 +74,12 @@ fetch_client_data () {
     
     if [ $? -eq 0 ]; then
         echo "####################################################"
-        echo "***Bearer token successfully fetched***"
+        echo "*** Bearer token successfully fetched ***"
         echo "####################################################"
         echo ""
     else 
         echo "####################################################"
-        echo "***Error while fetching bearer token***"
+        echo "*** Error while fetching bearer token ***"
         echo "Please check the keycloak admin credentials and other details"
         echo "####################################################"
         echo ""
@@ -86,7 +92,7 @@ fetch_client_data () {
    
     if [ $? -ne 0 ]; then
         echo "####################################################"
-        echo "***Error while fetching client***"
+        echo "*** Error while fetching client ***"
         echo "Provided realm is $KEYCLOAK_REALMS, please verify?"
         echo "####################################################"
         echo ""
@@ -95,12 +101,13 @@ fetch_client_data () {
 
 
     client_check=( $( printf '%s\n' "${KEYCLOAK_CLIENT_NAMES[@]}" | grep -i "$APP_NAME" ) )
+    echo $client_check
     if [ ! -z "$client_check" ]; then
         client_id=$(echo $clients | jq -r --arg kc ${client_check} '.[] | select(.clientId==$kc) | .id' )
         
         if [ $? -ne 0 ]; then
         echo "####################################################"
-        echo "***Error while fetching client ID***"
+        echo "*** Error while fetching client ID ***"
         echo "Provided Client name is $client_check, please verify?"
         echo "####################################################"
         echo ""
@@ -108,7 +115,7 @@ fetch_client_data () {
         fi
 
         echo "####################################################"
-        echo "*** $client_check ID successfully fetched***"
+        echo "*** $client_check ID successfully fetched ***"
         echo "####################################################"
         echo ""
 
@@ -117,7 +124,7 @@ fetch_client_data () {
         client_secret=$(echo $secret_data | jq -r .value)
 
         echo "####################################################"
-        echo "*** $client_check secret successfully fetched***"
+        echo "*** $client_check secret successfully fetched ***"
         echo "####################################################"
         echo ""
     
@@ -130,30 +137,59 @@ fetch_client_data () {
 
 prep_vault_auth () {
 
-    SA_Token=$(cat /var/run/secrets/kubernetes.io/serviceaccount/token)
-    SA_CA=$(cat /var/run/secrets/kubernetes.io/serviceaccount/ca.crt)
+    SA_Token=$(cat ./token)
+    SA_CA=$( cat ./ca.crt)
+
+    #SA_Token=$(cat /var/run/secrets/kubernetes.io/serviceaccount/token)
+    #SA_CA=$(cat /var/run/secrets/kubernetes.io/serviceaccount/ca.crt)
 
     SA_CERT=$(echo "$SA_CA" | awk 'NF {sub(/\r/, ""); printf "%s\\n",$0;}' )
 
-
     #############create lila-acl policy###############
-    check_policy=$(url --request GET --header "X-Vault-Token: $VAULT_TOKEN" $VAULT_PROTOCOL://$VAULT_URL/v1/sys/policy/lila-acl-policy | jq -r .name )
+    check_policy=$(curl -s --request GET --header "X-Vault-Token: $VAULT_TOKEN" $VAULT_PROTOCOL://$VAULT_URL/v1/sys/policy/lila-acl-policy | jq -r .name )
     if [ "$check_policy"  == "null" ]; then
         curl -s --request POST --header "X-Vault-Token: $VAULT_TOKEN" $VAULT_PROTOCOL://$VAULT_URL/v1/sys/policy/lila-acl-policy --data '{"policy":"path \"livinglab/'"*"'\" {\n capabilities = [\"list\",\"read\"]\n}"}'
+        echo "####################################################"
+        echo "*** created lila-vault-policy ***"
+        echo "####################################################"
+        echo ""
+    else
+        echo "####################################################"
+        echo "*** lila-vault-policy alread exists ***"
+        echo "####################################################"
+        echo ""
     fi
     ####################################################
 
     ###############enable kubernetes auth###############
     k8s_auth_enabled=$(curl -s --request GET --header "X-Vault-Token:  $VAULT_TOKEN" $VAULT_PROTOCOL://$VAULT_URL/v1/sys/auth/kubernetes  | jq -r .type)
     if [ "$k8s_auth_enabled"  == "null" ]; then
-        curl -s --request POST --header "X-Vault-Token:  $VAULT_TOKEN" --data '{"type":"kubernetes","description":"kubernetes auth"}'  $VAULT_PROTOCOL://$VAULT_URL/v1/sys/auth/kubernetes 
+        curl -s --request POST --header "X-Vault-Token:  $VAULT_TOKEN" --data '{"type":"kubernetes","description":"kubernetes auth"}'  $VAULT_PROTOCOL://$VAULT_URL/v1/sys/auth/kubernetes
+        echo "####################################################"
+        echo "*** enabled kubernetes auth ***"
+        echo "####################################################"
+        echo ""
+    else
+        echo "####################################################"
+        echo "*** kubernetes auth alread exists ***"
+        echo "####################################################"
+        echo ""
     fi
     ####################################################
    
     ###############create lila-vault-role###############
-    lila_role_exists=$(url -s --request GET --header "X-Vault-Token:  $VAULT_TOKEN" $VAULT_PROTOCOL://$VAULT_URL/v1/auth/kubernetes/role/lila-vault-role | jq -r .request_id )
+    lila_role_exists=$(curl -s --request GET --header "X-Vault-Token:  $VAULT_TOKEN" $VAULT_PROTOCOL://$VAULT_URL/v1/auth/kubernetes/role/lila-vault-role | jq -r .request_id )
     if [ "$lila_role_exists"  == "null" ]; then
         curl -s --request POST --header "X-Vault-Token:  $VAULT_TOKEN" --request POST --data '{ "bound_service_account_names" : ["lila-grafana-sa", "lila-argocd-sa","lila-daps-sa","lila-edc-sa","lila-mvd-sa","lila-datadashboard-sa","mvd-connector-deployment-edc-mvd-vault","keycloak-sa","argocd-sa","grafana-sa","tibco-sa"], "bound_service_account_namespaces" : ["grafana","livinglab-argocd","daps","edc","mvd","datadashboard","livinglab-grafana","livinglab-keycloak","tibco"] , "policies" : ["lila-acl-policy"]}' $VAULT_PROTOCOL://$VAULT_URL/v1/auth/kubernetes/role/lila-vault-role
+        echo "####################################################"
+        echo "*** created lila-vault-role ***"
+        echo "####################################################"
+        echo ""
+    else
+        echo "####################################################"
+        echo "*** lila-vault-role alread exists ***"
+        echo "####################################################"
+        echo ""
     fi
     ####################################################
 
@@ -161,6 +197,15 @@ prep_vault_auth () {
     config_exists=$(curl -s --header "X-Vault-Token: $VAULT_TOKEN" --request GET  $VAULT_PROTOCOL://$VAULT_URL/v1/auth/kubernetes/config | jq -r .request_id )
     if [ "$config_exists"  == "null" ]; then
         curl -s --header "X-Vault-Token: $VAULT_TOKEN" --request POST --data '{ "kubernetes_host": "http://kubernetes.default.svc.local", "kubernetes_ca_cert": "'"$SA_Token"'", "pem_keys": "'"$SA_CERT"'" }' $VAULT_PROTOCOL://$VAULT_URL/v1/auth/kubernetes/config
+        echo "####################################################"
+        echo "*** created kubernetes auth config ***"
+        echo "####################################################"
+        echo ""
+    else
+        echo "####################################################"
+        echo "*** kubernetes auth alread exists ***"
+        echo "####################################################"
+        echo ""
     fi
     ####################################################
 
@@ -170,12 +215,49 @@ prep_vault_auth () {
 #Function to POST data into vault
 post_vault_data () {
 
-    path=( $( printf '%s\n' "${VAULT_PATH[@]}" | grep -i "$1" ) )
 
-    if [ ! -z "$path" ] && [ "$path" != "database" ]; then
-        Post_Data=$(curl -s -H "X-Vault-Token: $VAULT_TOKEN" -H "Content-Type: application/json" -X POST -d '{"data":{"client-id":"'"$client_check"'","client-secret":"'"$client_secret"'"}}' "$VAULT_PROTOCOL://$VAULT_URL/v1/secret/data/$path")
-    elif [ ! -z "$path" ] && [ "$path" == "database" ]; then
-        Post_Data=$(curl -s -H "X-Vault-Token: $VAULT_TOKEN" -H "Content-Type: application/json" -X POST -d '{"data":{"password":"'"$client_secret"'"}}' "$VAULT_PROTOCOL://$VAULT_URL/v1/secret/data/$path")
+    ###############create kv2 secret path###############
+    secret_exists=$(curl -s --request GET --header "X-Vault-Token:  $VAULT_TOKEN" $VAULT_PROTOCOL://$VAULT_URL/v1/sys/mounts/livinglab  | jq -r .config)
+    if [ "$secret_exists" == "null" ]; then
+        curl -s --header "X-Vault-Token: $VAULT_TOKEN" --request POST --data '{"type":"kv","config" : {"default_lease_ttl":0,"force_no_cache":false,"max_lease_ttl":0}, "options": {"version": "2"}}' $VAULT_PROTOCOL://$VAULT_URL/v1/sys/mounts/livinglab
+        echo "####################################################"
+        echo "*** created KV2 secret ***"
+        echo "####################################################"
+        echo ""
+    else
+        echo "####################################################"
+        echo "*** KV2 secret alread exists ***"
+        echo "####################################################"
+        echo ""
+    fi
+    ####################################################
+
+   #####POST DATA INTO SUB-PATH######
+
+   if [[ "$APP_NAME" == *"argo"* ]]; then
+        #argo_data_exists=$(curl -s --request GET --header "X-Vault-Token: $VAULT_TOKEN" $VAULT_PROTOCOL://$VAULT_URL/v1/livinglab/data/argocd/argocd-oidc-secret| jq -r '.data[].client_id' )
+        if [ "$argo_data_exists" == "null" ]; then
+            curl -s -o /dev/null --request POST --header "X-Vault-Token:  $VAULT_TOKEN" --data '{"data":{"client_id":"'"$client_check"'","client_secret":"'"$client_secret"'"}}' $VAULT_PROTOCOL://$VAULT_URL/v1/livinglab/data/argocd/argocd-oidc-secret
+            curl -s -o /dev/null --request POST --header "X-Vault-Token:  $VAULT_TOKEN" --data '{"data":{"client_id":"'"$client_check"'","client_secret":"'"$client_secret"'"}}' $VAULT_PROTOCOL://$VAULT_URL/v1/livinglab/data/argocd/argocd-vault-secret
+            echo "####################################################"
+            echo "*** Added ARGO Data into vault ***"
+            echo "####################################################"
+            echo ""
+        fi
+    elif [[ "$APP_NAME" == *"grafana"* ]]; then
+        curl -s -o /dev/null --request POST --header "X-Vault-Token:  $VAULT_TOKEN" --data '{"data":{"client_id":"'"$client_check"'","client_secret":"'"$client_secret"'"}}' $VAULT_PROTOCOL://$VAULT_URL/v1/livinglab/data/grafana
+        echo "####################################################"
+        echo "*** Added ARGO Grafana into vault ***"
+        echo "####################################################"
+        echo ""
+    elif [[ "$APP_NAME" == *"kubeapps"* ]]; then
+        curl -s -o /dev/null --request POST --header "X-Vault-Token:  $VAULT_TOKEN" --data '{"data":{"client_id":"'"$client_check"'","client_secret":"'"$client_secret"'"}}' $VAULT_PROTOCOL://$VAULT_URL/v1/livinglab/data/kubeapps/keycloak-secret  
+        echo "####################################################"
+        echo "*** Added ARGO Data into vault ***"
+        echo "####################################################"
+        echo ""
     fi
 
 }
+
+fetch_client_data
